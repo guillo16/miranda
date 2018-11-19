83c0ba99ed8466fc52fbe05de4977212c9ab6c01
guillermo varela <guillovarela16@gmail.com>
2018-11-19 18:46:28 -0300

    update the routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'videos#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :videos, only: [:show, :index] do
-    resources :questions, only: [:show, :index] do
-      resources :answers, only: [:show, :index]
-    end
+  resources :videos, only: :show
+
+  resources :users, only: :show do
+    resources :bookmarks, only: [:index, :create]
   end
-  resources :users do
-    resources :bookmarks
-  end
+  resources :bookmarks, only: :destroy
 end
