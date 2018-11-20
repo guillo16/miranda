e2c73cac694db56c9639251a558d4be68197c384
guillermo varela <guillovarela16@gmail.com>
2018-11-20 13:11:49 -0300

    marketing email finish

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ApplicationMailer
   def welcome(user)
     @user = user # Instance variable => available in view
 
-    mail(to: @user.email, subject: 'Welcome to Le Wagon')
+    mail(to: @user.email, subject: 'Welcome to Miranda')
     # This will render a view in `app/views/user_mailer`!
   end
 
