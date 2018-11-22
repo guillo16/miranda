a7f17ab86bfb341b9159261153d8be2ec1f0fbfe
guillermo varela <guillovarela16@gmail.com>
2018-11-22 12:06:53 -0300

    change the devise redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,9 @@ class ApplicationController < ActionController::Base
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
 
+  protected
+
+  def after_sign_in_path_for(resource)
+    videos_path
+  end
 end
