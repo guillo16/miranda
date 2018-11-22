a2d1bc69107f0e157aab6e39015494bf0b1b8cfb
guillermo varela <guillovarela16@gmail.com>
2018-11-22 11:45:48 -0300

    change the routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "registrations", omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :videos, only: :show
+  resources :videos, only: [:show, :index]
 
   # resources :users, only: :show do
     resources :bookmarks, only: [:index, :create]
