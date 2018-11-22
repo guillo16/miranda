2169bd45d626df36d33782ec6fc6ec94a43d9036
Colin Strachan <col@strachanmusic.com>
2018-11-22 16:06:10 -0300

    added profile page and edit functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,26 @@ class UsersController < ApplicationController
     @user = current_user
     authorize @user
   end
+
+  def edit_profile
+    @user = current_user
+    authorize @user
+  end
+
+  def update_profile
+    @user = User.find(current_user.id)
+
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      render "edit"
+    end
+    authorize @user
+  end
+
+  private
+
+  def user_params
+    params.required(:user).permit(:first_name, :last_name, :photo, :email)
+  end
 end
