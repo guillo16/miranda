3df7a8c759b5dbfb0543a85003321db6816f186c
Colin Strachan <col@strachanmusic.com>
2018-11-23 15:03:32 -0300

    updated profile page with tabs

---
+++ app/policies/bookmark_policy.rb
@@ -0,0 +1,15 @@
+class BookmarkPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.where(user == current_user)
+    end
+  end
+
+  def create?
+    true
+  end
+
+  def destroy?
+    true
+  end
+end
