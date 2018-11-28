6b4d78142e1e92027dab94b62a5ce798559b5feb
guillermo varela <guillovarela16@gmail.com>
2018-11-28 18:15:52 -0300

    new file

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -6,8 +6,8 @@ class ChallengesController < ApplicationController
   def create
     @challenge = Challenge.new(challenges_params)
     @challenge.user = current_user
-    @challenge.save
     byebug
+    @challenge.save
     # @opponent = User.find(@challenge.opponent_id)
     authorize @challenge
   end
--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,5 +1,6 @@
 class Answer < ApplicationRecord
   belongs_to :question
+  has_many :answer_users
   validates :answer, presence: true
   validates :correct, :inclusion => { :in => [true, false] }
 end
